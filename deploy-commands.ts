1583e1bc923652bb4623371f2b064b0109441f80
Thoumey <thomas.borgelin@gmail.com>
2022-07-21 18:53:49 +0300

    init slashcommand builder

---
+++ deploy-commands.ts
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import { REST } from '@discordjs/rest';
+import { Routes } from 'discord-api-types/v10';
+import config from './config.json';
+
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(config.token);
+
+rest.put(Routes.applicationGuildCommands(config.clientId, config.guildId), { body: commands })
+	.then(() => console.log('Successfully registered application commands.'))
+	.catch(console.error);
